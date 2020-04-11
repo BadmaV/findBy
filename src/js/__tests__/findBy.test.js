3891a62917f24ef1d86c33c3894704e664dbdf9d
Badma <andraev@ya.ru>
2020-04-11 14:13:43 +0300

    task is ready

--- src/js/__tests__/findBy.test.js
+++ src/js/__tests__/findBy.test.js
@@ -4,27 +4,22 @@ const arr = [
   { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
   { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
   { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
-  { name: 'удача', type: 'attack', description: 'Повышает суперспособность' },
 ];
 
-const severalResults = [
-  { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
-  { name: 'удача', type: 'attack', description: 'Повышает суперспособность' },
+const finder = [
+  { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' }
 ];
 
 test('find by item', () => {
-  const finder1 = findBy('type', 'attack');
-  const finder2 = findBy('type');
+  const finder1 = findBy('name', 'маг');
+  const finder2 = findBy('name');
   const finder3 = findBy();
-  const finder4 = findBy('type', 'attack', 'name', 'урон');
 
   const result1 = arr.filter(finder1);
   const result2 = arr.filter(finder2);
   const result3 = arr.filter(finder3);
-  const result4 = arr.filter(finder4);
 
-  expect(result1).toEqual(severalResults);
+  expect(result1).toEqual(finder);
   expect(result2).toEqual([]);
-  expect(result3[0].type).toEqual('character');
-  expect(result4[0].name).toEqual('заклинание');
+  expect(result3[0].name).toEqual('маг');
 });
